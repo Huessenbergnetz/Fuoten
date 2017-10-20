9280a4aa3ccc2c315c551a9b2ec827739b9d86e1
Buschmann <kontakt@buschmann23.de>
2017-10-20 20:14:29 +0200

    [General] do not save locations in .ts files

--- updatel10n.sh
+++ updatel10n.sh
@@ -4,4 +4,4 @@ if [ ! -d translations ]; then
     mkdir translations
 fi
 
-lupdate-qt5 -no-obsolete -source-language en -target-language en -locations relative common sailfishos -ts translations/fuoten.ts
+lupdate-qt5 -no-obsolete -source-language en -target-language en -locations none common sailfishos -ts translations/fuoten.ts
