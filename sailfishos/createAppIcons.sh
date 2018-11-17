445fa0534eacc1d9445d77d648f93d3e366926f0
Buschmann <kontakt@buschmann23.de>
2018-11-17 13:58:30 +0100

    [SailfishOS] reorganize creation of icons

---
+++ sailfishos/createAppIcons.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+BASENAME=harbour-fuoten
+ICONSDIR=icons
+
+if [ ! -x /usr/bin/inkscape ]
+then
+    echo "Can not find inkscape executable"
+    exit 1
+fi
+
+SVGFILE=$BASENAME.svg
+
+if [ ! -r $SVGFILE ]
+then
+    echo "Can not find SVG source file!"
+    exit 1
+fi
+
+for SIZE in 86 108 128 150 172
+do
+    SIZEDIR=$ICONSDIR/${SIZE}x${SIZE}
+    FULLPATH=$SIZEDIR/$BASENAME.png
+
+    if [ ! -r $FULLPATH ]
+    then
+
+        if [ ! -d $SIZEDIR ]
+        then
+            mkdir -p $SIZEDIR
+        fi
+
+        FNAME=$(mktemp)
+        inkscape -z -e $FNAME -w $SIZE  -h $SIZE $SVGFILE &> /dev/null
+        if [ -x /usr/bin/zopflipng ]
+        then
+            zopflipng -y --iterations=500 --filters=01234mepb --lossy_transparent $FNAME $FULLPATH
+            rm $FNAME
+        else
+            mv $FNAME $FULLPATH
+        fi
+
+    fi
+done
+
+if [ ! -r $BASENAME.png ]
+then
+    if [ -r $ICONSDIR/86x86/$BASENAME.png ]
+    then
+        cp $ICONSDIR/86x86/$BASENAME.png $BASENAME.png
+    else
+        echo "Can not find 86x86 icon!"
+        exit 1
+    fi
+fi
---
+++ sailfishos/icons/createIcons.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+SCALES="1 1.25 1.5 1.75 2"
+SIZES="32 64 96"
+
+if [ ! -x /usr/bin/inkscape ]
+then
+    echo "Can not find inkscape executable"
+    exit 1
+fi
+
+for SCALE in $SCALES
+do
+    SCALEDIR="z$SCALE"
+
+    if [ ! -d $SCALEDIR ]
+    then
+        echo "Creating directory $SCALEDIR"
+        mkdir $SCALEDIR
+    fi
+done
+
+processSvg() {
+    SCALE=$1
+    OUTSIZE=$2
+    SVGFILE=$3
+
+    SCALEDIR="z$SCALE"
+
+    FILENAME=$(basename -- "$SVGFILE")
+    BASENAME="${FILENAME%.*}"
+
+    if [[ ($BASENAME == "lock-"* || $BASENAME == "cover-"*) && $OUTSIZE != 32 ]]
+    then
+        exit 0
+    fi
+
+    SIZE=`echo "$SCALE*$OUTSIZE" | bc`
+    SIZE=`LC_ALL=C printf '%.*f' 0 $SIZE`
+
+    if [[ $BASENAME == "lock-"* || $BASENAME == "cover-"* ]]
+    then
+        FNAME=icon-$BASENAME.png
+    else
+        if [ $OUTSIZE -eq 32 ]
+        then
+            FNAME=icon-s-$BASENAME.png
+        fi
+
+        if [ $OUTSIZE -eq 64 ]
+        then
+            FNAME=icon-m-$BASENAME.png
+        fi
+
+        if [ $OUTSIZE -eq 96 ]
+        then
+            FNAME=icon-l-$BASENAME.png
+        fi
+    fi
+
+    if [ -r $SCALEDIR/$FNAME -a -s $SCALEDIR/$FNAME ]
+    then
+        echo "$SCALEDIR/$FNAME already exists. Doing nothing."
+    else
+        echo "Creating $SCALEDIR/$FNAME (${SIZE}x${SIZE})"
+
+        inkscape -z -e $SCALEDIR/$FNAME -w $SIZE -h $SIZE $SVGFILE &> /dev/null
+        if [ -x /usr/bin/zopflipng ]
+        then
+            zopflipng -y --iterations=500 --filters=01234mepb --lossy_transparent $SCALEDIR/$FNAME $SCALEDIR/$FNAME
+        fi
+    fi
+}
+export -f processSvg
+
+
+if [ "$1" == "" ]; then
+
+    SVGFILES=`ls src/*.svg`
+
+    if [ -x /usr/bin/parallel ]
+    then
+        parallel processSvg ::: $SCALES ::: $SIZES ::: $SVGFILES
+    else
+        for SCALE in $SCALES
+        do
+            for SIZE in $SIZES
+            do
+                for SVGFILE in $SVGFILES
+                do
+                    processSvg $SCALE $SIZE $SVGFILE
+                done
+            done
+        done
+    fi
+
+else
+
+    if [ -x /usr/bin/parallel ]
+    then
+        parallel processSvg ::: $SCALES ::: $SIZES ::: $1
+    else
+        for SCALE in $SCALES
+        do
+            for SIZE in $SIZES
+            do
+                processSvg $SCALE $SIZE $1
+            done
+        done
+    fi
+fi
