69885c299cd7706bf8b5cd1c58966b564375e883
Buschmann <mf@huessenbergnetz.de>
2019-04-30 17:02:37 +0200

    [General] build Chinese (China) translations

--- releasel10n.sh
+++ releasel10n.sh
@@ -2,7 +2,7 @@
 
 lrelease-qt5 -idbased translations/fuoten.ts
 
-for LANG in da de en_GB en_US fr nl sv
+for LANG in da de en_GB en_US fr nl sv zh_CN
 do
 lrelease-qt5 -idbased translations/fuoten_$LANG.ts
 done
