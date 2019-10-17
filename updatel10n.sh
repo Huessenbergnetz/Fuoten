2d948d41100c66aa22cef2a1e7443682968d3dcd
Buschmann <mf@huessenbergnetz.de>
2019-10-17 12:59:54 +0200

    [General] improve translation extraction and release scripts

--- updatel10n.sh
+++ updatel10n.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-if [ ! -d translations ]; then
-    mkdir translations
+LUPDATE=`which lupdate-qt5`
+if [ ! -x $LUPDATE ]
+then
+    LUPDATE=`which lupdate`
 fi
 
-lupdate-qt5 -no-obsolete -source-language en -target-language en -locations none common sailfishos -ts translations/fuoten.ts
+if [ -x $LUPDATE ]
+then
+    echo "Found lupdate at $LUPDATE"
+else
+    echo "Can neither find lupdate-qt nor lupdate"
+    exit 1
+fi
+
+if [ ! -d translations ]; then
+    mkdir translations
+    if [ ! -d translations ]; then
+        echo "Failed to create translations directory!"
+        exit 1
+    fi
+fi
+
+$LUPDATE -no-obsolete -source-language en -target-language en -locations none common sailfishos -ts translations/fuoten.ts
