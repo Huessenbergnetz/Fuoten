f403f6e998f07fc4337a30296d621541ff4c6883
Buschmann <mf@huessenbergnetz.de>
2022-03-06 17:19:50 +0100

    [SFOS] translation  updates

--- sailfishos/translations/fuoten_sv.ts
+++ sailfishos/translations/fuoten_sv.ts
@@ -1425,22 +1425,22 @@
     <message id="fuoten-jssupport-default">
         <source>Default (%1)</source>
         <extracomment>Selectable entry in the combo box to choose JavaScript support, %1 will be replaced by the default value</extracomment>
-        <translation type="unfinished"/>
+        <translation>Standard (%1)</translation>
     </message>
     <message id="fuoten-settings-defaults-section">
         <source>Default settings</source>
         <extracomment>section header on the settings page</extracomment>
-        <translation type="unfinished"/>
+        <translation>Standardinställningar</translation>
     </message>
     <message id="fuoten-cookiebehavior-default">
         <source>Default (%1)</source>
         <extracomment>Selectable entry in the combo box to choose cookie behavior %1 will be replaced by the default value</extracomment>
-        <translation type="unfinished"/>
+        <translation>Standard (%1)</translation>
     </message>
     <message id="fuoten-open-article-default">
         <source>Default (%1)</source>
         <extracomment>Selectable entry in the combo box to choose how to open articles %1 will be replaced by the default value</extracomment>
-        <translation type="unfinished"/>
+        <translation>Standard (%1)</translation>
     </message>
 </context>
 </TS>
